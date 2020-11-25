1f7836067d334d1b8ddbe199843c972cb8673f9b
Alberto <aljofloro@gmail.com>
2020-11-24 20:06:49 -0500

    Initial commit

---
+++ app/src/main/java/com/miempresa/retrofitexample/Todo.java
@@ -0,0 +1,24 @@
+package com.miempresa.retrofitexample;
+
+public class Todo {
+  private int userId;
+  private int id;
+  private String title;
+  private Boolean completed;
+
+  public int getUserId() {
+    return userId;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public Boolean getCompleted() {
+    return completed;
+  }
+}
